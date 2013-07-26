fe85139079ee0546bce3078212f5299fd18595ea
Daniel Kalmar <kalmard@gmail.com>
2013-07-26 12:59:00 +0200

    Added meta file checks for deletion and added invalid filename checks (Thumbs.db).

--- pre-commit.py
+++ pre-commit.py
@@ -8,9 +8,9 @@ from sets import Set
 
 AssetDir = "Assets/"
 MetaDataExtension = ".meta"
+InvalidFiles = Set(["Thumbs.db"])
 
 def svnlook_find(repo, txn, changeType):
-	"""Provide list of files changed in txn of repo"""
 	svnlook = "/usr/bin/svnlook"
 	cmd = "'%s' changed -t '%s' '%s'" % (svnlook, txn, repo)
 	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -22,13 +22,9 @@ def svnlook_find(repo, txn, changeType):
 			break
 		line = line.decode().strip()
 		text_mod = line[0:1]
-		# Only if the contents of the file changed (by addition or update)
-		# directories always end in / in the svnlook changed output
 		if text_mod == changeType:
 			changed.append(line[4:])
 
-	# wait on the command to finish so we can get the
-	# returncode/stderr output
 	data = p.communicate()
 	if p.returncode != 0:
 		sys.stderr.write(data[1].decode())
@@ -56,21 +52,40 @@ def enforce_metadata(changes):
 	for file in files:
 		metaName = file + MetaDataExtension
 		if (not metaName in metas):
-			print file + " commited but .meta missing!"
+			print "Change for " + file + " commited but .meta missing!"
 			ret = False
 		else:
 			metas.remove(metaName)
 	
 	for meta in metas:
 		ret = False
-		print meta + " commited but file missing!"
+		print "Change for " + meta + " commited but file missing!"
+
+	return ret
+
+def validate_files(changes):
+	ret = True
+	for change in changes:
+		slashIdx = change.rfind("/")
+		if slashIdx >= 0:
+			fileName = change[slashIdx + 1:]
+			if fileName in InvalidFiles:
+				print fileName + " is invalid, don't commit it!"
+				ret = False
 
 	return ret
 
 def main(repo, txn):
+	ret = True
 	added = svnlook_find(repo, txn, "A")
 	if not enforce_metadata(added):
-		return 1
+		ret = False
+	if not validate_files(added):
+		ret = False
+
+	deleted = svnlook_find(repo, txn, "D")
+	if not enforce_metadata(deleted):
+		ret = False
 	return 1
 	
 
